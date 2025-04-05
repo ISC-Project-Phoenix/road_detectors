59a7dad8009ff2c62c16f666b5472ec4dd1853a3
= <=>
2025-04-05 14:05:45 -0400

    init
    Signed-off-by: = <=>

--- obj_detector_cv/obj_detector_cv/cv_subscriber.py
+++ obj_detector_cv/obj_detector_cv/cv_subscriber.py
@@ -8,8 +8,10 @@ import cv2
 from ultralytics import YOLO
 import numpy as np
 import time
-from .cv_backend import process_videos
-# from 
+from obj_detector_cv.cv_backend import process_videos
+from  phnx_msgs.msg import Contours
+from geometry_msgs.msg import Vector3
+
 
 
 class CVsubscriberNode(Node):
@@ -21,7 +23,8 @@ class CVsubscriberNode(Node):
 
         # ROS2 Image Publisher (processed output)
         # self.publisher = self.create_publisher(Image, 'processed_frames', 10)
-        self.poly_coeff_publisher = self.create_publisher(Float32MultiArray, '/road/polynomial', 1)
+        # self.poly_coeff_publisher = self.create_publisher(Float32MultiArray, '/road/polynomial', 1)
+        self.contours_publisher = self.create_publisher(Contours, '/road/Contours', 1)
 
         # TODO: Figure out how to subscribe correctly to compressed image
         # TODO: uncomment and fix
@@ -67,13 +70,39 @@ class CVsubscriberNode(Node):
         average_coeffs = (left_coeffs + right_coeffs) / 2.0
 
         # make the custom msg and publich coefficients and contours
+        # Process left contours
+        vector3d_left_contours = []
+        if left_contours:
+            for contour in left_contours:
+                reshaped = contour.reshape(-1, 2)  # Convert to Nx2 array
+                for point in reshaped:
+                    vec = Vector3()
+                    vec.x = float(point[0])  # x coordinate
+                    vec.y = float(point[1])  # y coordinate
+                    vec.z = 0.0
+                    vector3d_left_contours.append(vec)
 
+        # Process right contours (same approach)
+        vector3d_right_contours = []
+        if right_contours:
+            for contour in right_contours:
+                reshaped = contour.reshape(-1, 2)
+                for point in reshaped:
+                    vec = Vector3()
+                    vec.x = float(point[0])
+                    vec.y = float(point[1])
+                    vec.z = 0.0
+                    vector3d_right_contours.append(vec)
+
+        msg = Contours()
+        msg.left_contour = vector3d_left_contours
+        msg.right_contour = vector3d_right_contours
 
         # Create a ROS2 message and publish coefficients
         coeff_msg = Float32MultiArray()
         coeff_msg.data = average_coeffs.astype(float).tolist()
                          #.astype(float).flatten().tolist()
-        self.poly_coeff_publisher.publish(coeff_msg)
+        self.contours_publisher.publish(msg)
 
         # Log the coefficients
         self.get_logger().info(f"Published Polynomial Coefficients: {average_coeffs}")

4bce0a30fe60f4fde3175d31d13ad7ec2405c7a9
redto0 <aboccacc@umich.edu>
2025-06-06 11:44:55 -0400

    removed build files
    Signed-off-by: redto0 <aboccacc@umich.edu>

--- build/obj_detector_ai/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/fazal/Documents/dev/phnx_ws/src/road_detectors/install/obj_detector_ai'
